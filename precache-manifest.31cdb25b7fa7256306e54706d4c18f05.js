a3d1ed9d3c2c7c0b764572d4fb2fea3337816bfe
LeonShus <leonshustov1995@gmail.com>
2022-01-25 17:54:24 +0300

    Updates

---
+++ precache-manifest.31cdb25b7fa7256306e54706d4c18f05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfba0390e8b538fe77edff19705e7fe",
+    "url": "/Todolist-TS/index.html"
+  },
+  {
+    "revision": "388a6d528deeb3987a84",
+    "url": "/Todolist-TS/static/css/main.1caaf2f4.chunk.css"
+  },
+  {
+    "revision": "d489cc942e4f23920160",
+    "url": "/Todolist-TS/static/js/2.bcc79227.chunk.js"
+  },
+  {
+    "revision": "74e1f4f7c1ca7f0bbbe1d90a3dfe851a",
+    "url": "/Todolist-TS/static/js/2.bcc79227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388a6d528deeb3987a84",
+    "url": "/Todolist-TS/static/js/main.278337dd.chunk.js"
+  },
+  {
+    "revision": "8e1d9570a61c4111829b",
+    "url": "/Todolist-TS/static/js/runtime-main.9e872e3d.js"
+  }
+]);
